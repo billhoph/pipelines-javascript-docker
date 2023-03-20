fe156aaa0a332a54d8eb87d1ac403adb7969b7b3
billhoph <hojohn269@msn.com>
2023-03-21 02:20:39 +0800

    Update server.js

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Prisma Cloud Azure Devops Integration Demo for FWD')
+  res.send('Prisma Cloud Azure Devops Integration Demo for Customer xxx')
 })
 
 app.listen(port, () => {
